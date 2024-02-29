959c44995fc725906a6d5d871f4e745b628060ab
Amadeus Dabela Lanoa <amadeus.arjuna@gmail.com>
2024-02-28 22:51:10 -0300

    Souza Esporte Clube > Cruzeiro

--- almossar-server/test/MessageSpec.hs
+++ almossar-server/test/MessageSpec.hs
@@ -10,7 +10,7 @@ spec :: IO ()
 spec = hspec $ do
     describe "JSON encoding of Message" $ do
         it "encodes a Message correctly" $ do
-            let actual = toJSON $ Message "🦖" "Pode não!" ( read "2022-02-28 12:00:00 UTC" :: UTCTime )
-            let expected = "{\"emoji\":\"🦖\",\"message\":\"Pode não!\",\"timestamp\":\"2022-02-28T12:00:00Z\"}"
+            let actual = toJSON $ Message "🦖👍⚽️" "Pode não!" ( read "2022-02-28 12:00:00 UTC" :: UTCTime )
+            let expected = "{\"emoji\":\"🦖👍⚽️\",\"message\":\"Pode não!\",\"timestamp\":\"2022-02-28T12:00:00Z\"}"
 
             encode actual `shouldBe` expected
