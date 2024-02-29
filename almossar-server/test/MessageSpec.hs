498ecf551723eeea1a263ee00318eb6e46d6a3f7
Amadeus Dabela Lanoa <amadeus.arjuna@gmail.com>
2024-02-28 22:49:08 -0300

    Add Message Json encoder

---
+++ almossar-server/test/MessageSpec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module MessageSpec (spec) where
+
+import Test.Hspec
+import Data.Aeson
+import Data.Time
+import Message
+
+spec :: IO ()
+spec = hspec $ do
+    describe "JSON encoding of Message" $ do
+        it "encodes a Message correctly" $ do
+            let actual = toJSON $ Message "🦖" "Pode não!" ( read "2022-02-28 12:00:00 UTC" :: UTCTime )
+            let expected = "{\"emoji\":\"🦖\",\"message\":\"Pode não!\",\"timestamp\":\"2022-02-28T12:00:00Z\"}"
+
+            encode actual `shouldBe` expected
